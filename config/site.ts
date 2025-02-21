daea3c93b3c5b01926b7622f0b055178286f7a71
Scapranger <36254545+RefinedSaltqwe@users.noreply.github.com>
2025-02-21 11:57:40 -0600

    commit

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "ScapFX",
+  description: "@scapranger's presets and luts",
+  icon: "/favicon.ico",
+  theme_color: "#0d1117",
+  color_scheme: "dark only",
+  twitter_image: "/assets/images/preview-dark.jpg",
+  twitter_card: "summary_large_image",
+  og_url: "splash.org",
+  og_image: "/assets/images/preview-dark.jpg",
+  og_type: "website",
+};

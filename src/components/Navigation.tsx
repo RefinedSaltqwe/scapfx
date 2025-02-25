22a776aa116e97eee6db85e3cabe71ee2192a673
Scapranger <36254545+RefinedSaltqwe@users.noreply.github.com>
2025-02-24 18:05:42 -0600

    2025-02-24 - Fixing Checkout success

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,9 +5,13 @@ import { cn } from "@/lib/utils";
 import { Search, ShoppingBasket } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 const Navigation: React.FC = () => {
+  const pathname = usePathname();
+  const checkout_success_page = pathname.includes("checkout_success");
+
   const [isAtTop, setIsAtTop] = useState(true);
   const openCart = useCart((state) => state.onOpen);
   const presets = useCart((state) => state.presets);
@@ -57,13 +61,24 @@ const Navigation: React.FC = () => {
                     height={32}
                     className="rounded-md"
                   />
-                  <span className="text-primary-foreground text-2xl font-medium">
+                  <span
+                    className={cn(
+                      "text-primary-foreground text-2xl font-medium",
+                      checkout_success_page && "text-primary",
+                      !isAtTop && "text-primary-foreground!",
+                    )}
+                  >
                     scap.
                   </span>
                 </Link>
               </div>
 
-              <div className="flex flex-1 items-center justify-end">
+              <div
+                className={cn(
+                  "flex flex-1 items-center justify-end",
+                  checkout_success_page && "hidden",
+                )}
+              >
                 {/* Search */}
                 <Link href="#" className="text-primary-foreground p-2">
                   <span className="sr-only">Search</span>
@@ -71,7 +86,7 @@ const Navigation: React.FC = () => {
                 </Link>
 
                 {/* Cart */}
-                <div className="ml-4 flow-root lg:ml-8">
+                <div className={"ml-4 flow-root lg:ml-8"}>
                   <span
                     onClick={() => openCart()}
                     className="text-primary-foreground group -m-2 flex cursor-pointer items-center p-2"

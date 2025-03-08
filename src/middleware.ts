35217ce415a49d2f9d74928dbc8f2aaa6f86a41d
Scapranger <36254545+RefinedSaltqwe@users.noreply.github.com>
2025-03-08 17:16:59 -0600

    2025-03-08 - possible fix for middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,35 +2,38 @@ import { type NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const { pathname, origin, href } = req.nextUrl;
+  const cookies = req.cookies;
+  const sessionToken = cookies.get("__Secure-authjs.session-token.0");
+  const sessionToken01 = cookies.get("authjs.session-token.0");
+
+  console.log("session from middleware", !!sessionToken || !!sessionToken01);
 
   // Redirect /shop and root to /shop/zenith
   if (pathname === "/shop" || pathname === "/") {
     return NextResponse.redirect(`${origin}/shop/aether`);
   }
 
-  // Protect all /account/[userId] routes (including /purchase-history)
+  // If user is not logged in and tries to access account. Redirect to login page
   if (pathname.startsWith("/account/")) {
-    const cookies = req.cookies;
-    console.log("COOKIES IN MIDDLEWARE:", req.cookies.getAll()); // Debugging
-
-    // const token = await getToken({ req, secret: env.NEXTAUTH_SECRET });
-
-    const sessionToken = cookies.get("__Secure-authjs.session-token.0");
-    console.log("TOKEN IN MIDDLEWARE:", sessionToken); // Debugging
-
-    if (!sessionToken) {
+    //? If user is not logged in
+    if (sessionToken || sessionToken01) {
+      return NextResponse.next();
+    } else {
       console.log("Redirecting to login because token is null"); // Debugging
       const loginUrl = new URL("/login", req.nextUrl.origin);
       loginUrl.searchParams.set("callbackUrl", href);
       return NextResponse.redirect(loginUrl);
     }
-
-    // if (!cookies) {
-    //   console.log("Redirecting to login because token is null"); // Debugging
-    //   const loginUrl = new URL("/login", req.nextUrl.origin);
-    //   loginUrl.searchParams.set("callbackUrl", href);
-    //   return NextResponse.redirect(loginUrl);
-    // }
+  }
+  // If user is logged in and tries to access login. Redirect to store
+  if (pathname.startsWith("/login")) {
+    if (sessionToken || sessionToken01) {
+      const mainStore = new URL("/shop/aether", req.nextUrl.origin);
+      mainStore.searchParams.set("callbackUrl", href);
+      return NextResponse.redirect(mainStore);
+    } else {
+      return NextResponse.next();
+    }
   }
 
   return NextResponse.next();
@@ -38,5 +41,5 @@ export async function middleware(req: NextRequest) {
 
 // Apply middleware to /shop and all /account/[userId] pages (including purchase-history) , "/account", "/account/:path*"
 export const config = {
-  matcher: ["/shop", "/account", "/account/:path*"],
+  matcher: ["/shop", "/account", "/account/:path*", "/login"],
 };

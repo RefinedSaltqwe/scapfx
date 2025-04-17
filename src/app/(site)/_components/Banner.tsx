191b733f8fe80c592f2b9c0f4d99f49942d1424a
Scapranger <36254545+RefinedSaltqwe@users.noreply.github.com>
2025-04-17 11:29:43 -0600

    2025-04-17 - Banner adjustment

--- src/app/(site)/_components/Banner.tsx
+++ src/app/(site)/_components/Banner.tsx
@@ -5,12 +5,12 @@ const Banner: React.FC = () => {
     <div className="flex items-center justify-center bg-red-600 px-8 py-2.5 sm:px-3.5">
       <div className="flex flex-col items-center justify-center text-sm/6 text-white">
         <span className="flex text-xl font-bold md:hidden">
-          SPRING SALE – UP TO 70% OFF!
+          SPRING SALE – 70% OFF!
         </span>
         <span className="hidden text-xl font-bold md:flex">
-          SPRING SALE • UP TO 70% OFF • ENDS SOON
+          SPRING SALE • 70% OFF • ENDS SOON
         </span>
-        <span className="flex md:hidden">{`Ends soon. Grab your presets now before they're gone.`}</span>
+        <span className="flex text-center md:hidden">{`Ends soon. Don't miss out.`}</span>
       </div>
     </div>
   );

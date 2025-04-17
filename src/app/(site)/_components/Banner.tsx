2e502690280a3ae05743b4cd54c605b2f2a9e2d3
Scapranger <36254545+RefinedSaltqwe@users.noreply.github.com>
2025-04-17 11:31:37 -0600

    EASTER SALE

--- src/app/(site)/_components/Banner.tsx
+++ src/app/(site)/_components/Banner.tsx
@@ -5,10 +5,10 @@ const Banner: React.FC = () => {
     <div className="flex items-center justify-center bg-red-600 px-8 py-2.5 sm:px-3.5">
       <div className="flex flex-col items-center justify-center text-sm/6 text-white">
         <span className="flex text-xl font-bold md:hidden">
-          SPRING SALE – 70% OFF!
+          EASTER SALE – 70% OFF!
         </span>
         <span className="hidden text-xl font-bold md:flex">
-          SPRING SALE • 70% OFF • ENDS SOON
+          EASTER SALE • 70% OFF • ENDS SOON
         </span>
         <span className="flex text-center md:hidden">{`Ends soon. Don't miss out.`}</span>
       </div>
